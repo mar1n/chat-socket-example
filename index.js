368694c06bbe25a3aa2dc6f06ef66c5db7357c5f
mar1n <30738216+mar1n@users.noreply.github.com>
2021-01-20 16:51:28 +0000

    emitting events

--- index.js
+++ index.js
@@ -7,11 +7,10 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('disconnect', () => {
-      console.log('user disconnected');
-    });
-  });
+    socket.on('chat message', (msg) => {
+        console.log('message ' + msg);
+    })
+});
 
 http.listen(3000, () => {
     console.log('listening on *:3000');
