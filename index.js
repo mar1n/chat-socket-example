4f70e30d4a20443c2eecc0f5e6d6e02c347c9855
mar1n <30738216+mar1n@users.noreply.github.com>
2021-01-20 18:46:25 +0000

    send event from server the server to the client

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-const app = require('express')();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const content = require('fs').readFileSync(__dirname + '/index.html', 'utf8');
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+const httpServer = require('http').createServer((req, res) => {
+  // serve the index.html file
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Content-Length', Buffer.byteLength(content));
+  res.end(content);
 });
 
-io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
-    })
+const io = require('socket.io')(httpServer);
+
+io.on('connection', socket => {
+    let counter = 0;
+    setInterval(() => {
+        socket.emit('hello', ++counter);
+    }, 1000);
 });
 
-http.listen(3000, () => {
-    console.log('listening on *:3000');
+httpServer.listen(3000, () => {
+  console.log('go to http://localhost:3000');
 });
